5df07185730c17afe908e5f47cae40831f79b212
Luis Morales <luismc2211mail.com>
2022-09-28 15:53:50 -0500

    App, y cambios de carpeta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './componentes/Login';
+import Dashboard from './componentes/Dashboard'
+import Layout from './componentes/Layout';
+import RutaNoEncontrada from './componentes/RutaNoEncontrada';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+          <Route element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='*' element={<RutaNoEncontrada />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
+
 
 export default App;
