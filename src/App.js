cb6aecb90a584547d7728738146a9460d01dadfa
Luis Morales <luismc2211mail.com>
2022-10-03 20:04:13 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Inicio from './componentes/Inicio/Inicio';
 import Nosotros from './componentes/Nosotros/Nosotros';
-import Login from './componentes/Login';
+import Login from './componentes/Login/Login';
 import Dashboard from './componentes/Dashboard'
 import Layout from './componentes/Layout/Layout';
 import RutaNoEncontrada from './componentes/RutaNoEncontrada';
--- src/componentes/Login.js
+++
@@ -1,8 +0,0 @@
-const Login = () => {
-    return (
-        <main style={{ paddings: '20px' }}>
-            <h4>Página de Login</h4>
-        </main>
-    );
-};
-export default Login;
